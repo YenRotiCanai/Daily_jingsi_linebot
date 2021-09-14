a053e908c8a4a82cb44783818685ea4dac3aac09
YenRotiCanai <lnight94@gmail.com>
2021-09-14 22:45:56 +0800

    argh5

--- app/models_for_line.py
+++ app/models_for_line.py
@@ -5,7 +5,7 @@ import random
 # Phoebe愛唱歌
 @handler.add(MessageEvent, message=TextMessage)
 def echo(event):
-	if event.source.userid != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+	if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
 		line_bot_api.reply_message(
 			event.reply_token,
 			TextSendMessage(text=event.message.text)
