8a2a0c28f07c40e524d967edf624eb213c406036
YenRotiCanai <lnight94@gmail.com>
2021-09-17 17:12:54 +0800

    text

--- app/draw.py
+++ app/draw.py
@@ -8,12 +8,12 @@ def draw():
 
     response = requests.get("https://daily-jingsi.herokuapp.com/draw")
     soup = BeautifulSoup(response.content, "html.parser")
-    text = soup.find('h2').getText().replace(" ","").replace("\n","")
+    souptext = soup.find('h2').getText().replace(" ","").replace("\n","")
 
-    return text
+    return souptext
 
 @handler.add(MessageEvent, message=TextMessage)
 def reply_text(event):
-
+    text = draw()
     if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=text))
